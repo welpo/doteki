954a2f9d058d72024c49d1000a8eb96b89fbf86b
bcignasi <ignasi.buendia@gmail.com>
2024-02-18 22:37:58 +0100

    Removed test_proto2

--- tests/plugins/test_figlet.py
+++ tests/plugins/test_figlet.py
@@ -13,20 +13,6 @@ def test_prototype():
     assert expected in str(result)
 
 
-def test_proto_2():
-    import pyfiglet
-
-    expected = """ _           _       \n| |__   ___ | | __ _ \n| '_ \\ / _ \\| |/ _` |\n| | | | (_) | | (_| |\n|_| |_|\\___/|_|\\__,_|\n                     \n"""
-    result = pyfiglet.figlet_format("hola")
-
-    # expected = expected.replace(" ", "S").replace("\n","r")
-    # result = result.replace(" ", "S").replace("\n","r")
-
-    # print(repr(expected))
-    # print(repr(result))
-    assert result == expected
-
-
 # If no parameters, result is none
 # Test is superseeded by test_empty_text
 """def test_empty_settings(caplog):
